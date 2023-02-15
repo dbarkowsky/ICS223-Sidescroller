0d502588768e299d68d6793aa77bc9b74d459219
dbarkowsky <dylanbarkowsky@gmail.com>
2023-02-15 11:47:25 -0800

    added particle effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Player controls
     [SerializeField] private Rigidbody rb;
     private float jumpForce = 12f;
     private bool grounded = true;
 
+    // Game Manager
     [SerializeField] private GameManager gameManager;
 
+    // Animations
     [SerializeField] private Animator anim;
     private bool dead = false;
+
+    // Effects
+    [SerializeField] private ParticleSystem explosionParticle;
+    [SerializeField] private ParticleSystem dirtParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +34,7 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             grounded = false;
             anim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
 
@@ -35,12 +43,15 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             grounded = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.tag == "Obstacle")
         {
             gameManager.EndGame();
             anim.SetBool("Death_b", true);
             anim.SetInteger("DeathType_int", 2);
             dead = true;
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
 }
